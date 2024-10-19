95933800564f07dc5c6b10254e284362a4532911
Atharv Tamboli <tamboli.atharv@gmail.com>
2024-10-19 16:48:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('hovered');
+    });
+    card.addEventListener('mouseleave', () => {
+        card.classList.remove('hovered');
+    });
+});
