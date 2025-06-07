dd61056157bdc4ab2425cbc9b2f6b2d3b3d99e01
Atharv Tamboli <tamboli.atharv@gmail.com>
2025-06-08 03:02:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const projectCards = document.querySelectorAll('.project-card');
 
-projectCards.forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.classList.add('hovered');
-    });
-    card.addEventListener('mouseleave', () => {
-        card.classList.remove('hovered');
-    });
-});
